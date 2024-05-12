432d12273ed69ba62c5475df47eab738fb476c1b
Candice Cirbo <thecirbos@icloud.com>
2024-05-11 21:51:12 -0600

    Finished file

---
+++ lib/player.rb
@@ -0,0 +1,21 @@
+class Player
+   attr_reader :name,
+               :deck
+
+
+
+   def initialize(name, deck)
+    @name = name
+    @deck = deck
+   end
+
+   def has_lost?
+ 
+    if @deck.cards.length >= 1
+        false
+    else    
+        true
+    end    
+   end
+   
+end
